ac95f7ea7c132ad56a58bab7b91176d06bd7ee8a
huytang <68282036+huytang99@users.noreply.github.com>
2023-04-18 16:06:51 +0700

    fix cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const connect = async () => {//can only throw error when cannot make first conne
 
 }
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
